14f57d5d148432dd46c3b28117c3a669f6d1cb20
unknown <sstt@gmail.com>
2014-04-18 13:05:27 +0300

    kostyl for BacklogItem fetch from server

--- app/scripts/TeamCandidates/TeamCandidatesCollectionView.js
+++ app/scripts/TeamCandidates/TeamCandidatesCollectionView.js
@@ -15,7 +15,6 @@
             this.collection = new module.Collection(data["team_id"]);
             this.collection.on('sync', this.render, this);
 			this.collection.fetch(); /*fetch() from collection added here to boost performance*/
-			console.log(this.collection);
         },
 
         render: function() {
