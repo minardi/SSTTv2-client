97de2cbea074fe87f3cbf61b8093a95b915f11db
Andrey Granat <=>
2014-04-18 17:10:23 +0300

    remove app.ProductCollection

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -21,8 +21,6 @@ var sstt = {},
 $(function() {
     "use strict";
 
-    app.ProductBacklog.Collection = app.BacklogItem.Collection;
-
     sstt.user = new app.User.ModelView({
         el: $(".user"),
         user_content: current_user_content
