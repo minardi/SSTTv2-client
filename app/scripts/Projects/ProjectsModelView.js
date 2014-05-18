425b28d2d065abfd326ae86d750c05e403b091dc
VRoksolana <roksolana.velychenko@gmail.com>
2014-05-18 15:09:01 +0300

    check conflict in ProjectModelView

--- app/scripts/Projects/ProjectsModelView.js
+++ app/scripts/Projects/ProjectsModelView.js
@@ -18,21 +18,12 @@
         selectProject: function() {
             mediator.pub("ProjectPage:ProjectSelected", this.model.id);
         },
-<<<<<<< HEAD
-        
-        showProjectInfo: function() {
-            sstt.current_project = this.model.id;
-            mediator.pub("ProjectPage:ProjectChecked", this.model);
-            this.$el.siblings().removeClass("active-tab"); 
-            this.$el.addClass("active-tab"); 
-=======
-		
+
 		checkProject: function() {
 			sstt.current_project = this.model.id;
 			this.$el.siblings().removeClass("active-tab"); 
             this.$el.addClass("active-tab");
 			mediator.pub("ProjectPage:ProjectChecked", this.model, "project");
->>>>>>> a37ff20cb92f822996fe2a2b0fd39daf395dce57
         },
 
         render: function() {
