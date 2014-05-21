75a440fa89a5b61e2b3d1b77e19e10c438829679
George Bugackov <sstt@gmail.com>
2014-05-21 17:08:17 +0300

    sub-tasks in progress

--- app/scripts/SprintBacklog/SprintBacklogCollectionView.js
+++ app/scripts/SprintBacklog/SprintBacklogCollectionView.js
@@ -28,6 +28,7 @@
             _.bindAll(this, "restoreStory");
 
             this.collection = new module.Collection();
+			
             this.collection.on("add remove", this.checkFilling, this);
 
             this.sprint = new module.Model({}, {
@@ -35,7 +36,6 @@
             });
             this.sprint.fetch();
 
-            this.render();
         },
 
         stopSprint: function() {
@@ -54,37 +54,42 @@
 
         sprintWasSaved: function () {
             this.sprint.clear()
-                .on("change", this.saveAllStory, this)
+                .on("change", this.saveAllStories, this)
                 .fetch();
         },
 
-        saveAllStory: function (sprint) {
+        saveAllStories: function (sprint) {
             var story_parent_id = sprint.get("id");
             
             this.sprint.off("change");
+			
 
             this.collection.each(function (model) {
-                model.set("parent_id", story_parent_id);
-                model.set("status", "todo");
-                model.save(null,{
-                    success: _.bind(this.restoreStory, this)
-                });
+			
+				if (model.item_type === "story") {
+					model.set("parent_id", story_parent_id);
+					model.set("status", "sprint");
+					model.save(null,{
+						success: _.bind(this.restoreStory, this)
+					});
+				}
             }, this);
 
             this.$list.empty();
         },
-
-        addBacklogItem: function (story) {
-            this.collection.add(story);
-            this.renderOne(story);
+		
+        addBacklogItem: function (item) {
+		
+            this.collection.add(item);
+			this.renderOne(item);
         },
-
-        renderOne: function (backlogItem) {
+		
+        renderOne: function (item) {
             var backlogItemView = new module.ModelView({
-                    model: backlogItem
+                    model: item
                 });
 
-            this.$list.append(backlogItemView.render().el);
+			this.$list.append(backlogItemView.render().el);
         },
 
         restoreStory: function(story) {
--- app/scripts/SprintBacklog/SprintBacklogModelView.js
+++ app/scripts/SprintBacklog/SprintBacklogModelView.js
@@ -3,42 +3,66 @@
 (function(module) {
 
     module.ModelView = Backbone.View.extend({
-
-        className: "story-box",
-
+	
+		className: "story-wraper",
+		
+		selected: false,
+		
         template: {
 			"story": JST['app/scripts/SprintBacklog/SprintBacklogStoryTpl.ejs'],
 			"task":  JST['app/scripts/SprintBacklog/SprintBacklogTaskTpl.ejs']
 		},
+		
+		subscriptions: {
+		   //"ContextMenu:Configure": "edit",
+		   //"ContextMenu:Delete": "showConfirm",
+           //"ContextMenu:Back": "deselectAll",
+		   //"ContextMenu:Team": "deselectAll",
+		   "module:UnitSelected": "deselectAll",
+		   "BacklogItemEdit:TaskCreated": "renderTask",			
+        },
 
         events: {
         	"dblclick": "restoreToProduct",
-			"click .add-new-task": "openEditor"
+			"click .add-new-task": "openEditor",
+			"click": "storySelected"
         },
-		
-		subscriptions: {
-			"BacklogItemEdit:TaskCreated": "renderTask"
-		},
 
         restoreToProduct: function() {
         	this.remove();
         	mediator.pub("SprintBacklog:RestoreStory", this.model);
         },
 		
-		openEditor: function() {
+		openEditor: function() {		    
 			mediator.pub ("SprintBacklog:CreateTask", {item_type: "task", status: "todo", parent_id: this.model.get("id")});
 		},
 		
-		renderTask: function(task_model) {
-			console.log(task_model);
-			this.$el.append(this.template["task"](task_model.toJSON()));
+		 storySelected: function() {
+			//mediator.pub("SprintBacklog:SelectedStory");
+			mediator.pub("module:UnitSelected", this.model, "backlog_item");
+
+			this.selected = true;
+            this.$el.addClass('selected');
+        },
+		
+		deselectAll: function() {
+			this.selected = false;
+            this.$el.removeClass('selected');
 		},
 
         render: function() {
-            this.$el.html(this.template["story"](this.model.toJSON()));
-            
+		
+				this.$el.append(this.template["story"](this.model.toJSON())); 
+		
             return this;
-        }
+        },
+		
+		renderTask: function() {
+			
+			if (this.selected) {
+				this.$el.append(this.template["task"](this.model.toJSON()));
+			}
+		}
 
     });
 
